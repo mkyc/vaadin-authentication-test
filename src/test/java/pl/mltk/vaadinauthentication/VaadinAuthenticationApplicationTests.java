e7c3951d65fbcdc8a398847eacbb281ebec574be
Mateusz Kyc <mateusz.kyc@gmail.com>
2017-07-18 23:18:53 +0200

    init

---
+++ src/test/java/pl/mltk/vaadinauthentication/VaadinAuthenticationApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mltk.vaadinauthentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VaadinAuthenticationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
